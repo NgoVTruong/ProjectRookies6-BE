601b7b2541b0714c8f95c2c678f2c75973545f33
Bùi Quang Hưng <hung.buiquang@nashtechglobal.com>
2022-11-20 14:24:18 +0700

    Add code to update database automatically

--- FinalAssignment/Program.cs
+++ FinalAssignment/Program.cs
@@ -11,10 +11,32 @@ using System.Text;
 using FinalAssignment.Repositories.Interfaces;
 using FinalAssignment.Repositories.Implements;
 
+static async Task InitializeDatabase(IApplicationBuilder app)
+{
+    using var scope = app.ApplicationServices.GetService<IServiceScopeFactory>()?.CreateScope();
+    if (scope != null)
+    {
+        await scope.ServiceProvider.GetRequiredService<FinalAssignmentContext>().Database.MigrateAsync();
+    }
+}
+
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
 
 // Add services to the container.
+builder.Services.AddDbContext<FinalAssignmentContext>(options =>
+{
+    options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+});
 
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IAssetRepository, AssetRepository>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IAssignmentRepository, AssignmentRepository>();
+builder.Services.AddScoped<IRequestReturningRepository, RequestReturningRepository>();
+builder.Services.AddScoped<IReportRepository, ReportRepository>();
+
+// builder.Services.AddScoped<ITestService, TestService>();
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("MyCors",
@@ -26,46 +48,16 @@ builder.Services.AddCors(options =>
         });
 });
 
-builder.Services.AddControllers().AddJsonOptions(o =>
-        o.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve
-    );
+builder.Services.AddControllers()
+                .AddJsonOptions(options => options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve);
 
-builder.Services.AddControllers();
-
-var configuration = builder.Configuration;
-
-builder.Services.AddTransient<IUserService, UserService>();
-
-builder.Services.AddTransient<IAssetRepository, AssetRepository>();
-builder.Services.AddTransient<IUserService, UserService>();
-
-builder.Services.AddTransient<ICategoryRepository, CategoryRepository>();
-builder.Services.AddTransient<IUserService, UserService>();
-
-builder.Services.AddTransient<IAssignmentRepository, AssignmentRepository>();
-builder.Services.AddTransient<IUserService, UserService>();
-
-builder.Services.AddTransient<IRequestReturningRepository, RequestReturningRepository>();
-builder.Services.AddTransient<IUserService, UserService>();
-
-builder.Services.AddTransient<IReportRepository, ReportRepository>();
-builder.Services.AddTransient<IUserService, UserService>();
-
-builder.Services.AddDbContext<FinalAssignmentContext>(opt =>
-{
-    opt.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
-});
-
-/*
-builder.Services.AddTransient<ITestService, TestService>();
-*/
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
-    .AddEntityFrameworkStores<FinalAssignmentContext>()
-    .AddDefaultTokenProviders();
+                .AddEntityFrameworkStores<FinalAssignmentContext>()
+                .AddDefaultTokenProviders();
 
 // Adding Authentication
 builder.Services.AddAuthentication(options =>
@@ -74,26 +66,29 @@ builder.Services.AddAuthentication(options =>
     options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
     options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
 })
-.AddJwtBearer(options =>
-{
-    options.SaveToken = true;
-    options.RequireHttpsMetadata = false;
-    options.TokenValidationParameters = new TokenValidationParameters()
-    {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-        ClockSkew = TimeSpan.Zero,
+                .AddJwtBearer(options =>
+                {
+                    options.SaveToken = true;
+                    options.RequireHttpsMetadata = false;
+                    options.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ClockSkew = TimeSpan.Zero,
 
-        ValidAudience = builder.Configuration["JWT:ValidAudience"],
-        ValidIssuer = builder.Configuration["JWT:ValidIssuer"],
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Secret"]))
-    };
-});
+                        ValidAudience = builder.Configuration["JWT:ValidAudience"],
+                        ValidIssuer = builder.Configuration["JWT:ValidIssuer"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Secret"]))
+                    };
+                });
 
 
 var app = builder.Build();
+
+// Update database automatically
+InitializeDatabase(app).Wait();
 
 // Configure the HTTP request pipeline.
 app.UseSwagger();
