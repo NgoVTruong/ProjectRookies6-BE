56be27a8822a4609d4894262f0f926e9eb733048
Bùi Quang Hưng <hung.buiquang@nashtechglobal.com>
2022-11-21 15:04:21 +0700

    Update relationship between User and Assignment tables

--- Data/Migrations/20221121075836_Update_Relationships_Between_User_Assignment.Designer.cs
+++ Data/Migrations/20221121075836_Update_Relationships_Between_User_Assignment.Designer.cs
@@ -4,6 +4,7 @@ using Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Data.Migrations
 {
     [DbContext(typeof(FinalAssignmentContext))]
-    partial class FinalAssignmentContextModelSnapshot : ModelSnapshot
+    [Migration("20221121075836_Update_Relationships_Between_User_Assignment")]
+    partial class Update_Relationships_Between_User_Assignment
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
