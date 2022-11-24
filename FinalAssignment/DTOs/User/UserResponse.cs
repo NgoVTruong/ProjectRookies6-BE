7eb1bb4920583e997a7169ca9a4122d591eaed0a
Fannabykiki <namplhe153292@fpt.edu.vn>
2022-11-24 15:07:44 +0700

    resolve duplicate

--- FinalAssignment/DTOs/User/UserResponse.cs
+++ FinalAssignment/DTOs/User/UserResponse.cs
@@ -13,12 +13,10 @@ namespace FinalAssignment.DTOs.User
         public string StaffCode {get; set;}
         public string FullName {get; set;}
         public string UserName { get; set; }
-        public int? Gender { get; set; }
         public DateTime? JoinedDate { get; set; }
         public DateTime? DateOfBirth { get; set; }
         public string? Type { get; set; }
         public string Location { get; set; }
-        public DateTime DateOfBirth { get; set; }
         public GenderEnum Gender { get; set; }
     }
 }
