38bddde1b8e1ccce66a229a399288cba9b50d52c
nguyenthan88 <nguyenvanthanvp18@gmail.com>
2022-12-14 12:39:14 +0700

    update assignment

--- FinalAssignment/Services/Implements/ReportService.cs
+++ FinalAssignment/Services/Implements/ReportService.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using FinalAssignment.DTOs.Report;
 using FinalAssignment.Repositories.Interfaces;
 using FinalAssignment.Services.Interfaces;
